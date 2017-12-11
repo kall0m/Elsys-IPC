802b700783c5b28a86981f56e3a32fd2822b3b73
Kaloyan Madjunov <k.madjunov@gmail.com>
2017-12-11 12:52:59 -0800

    Day 418. I still don't get it and it doesn't work.

--- Grade5+/cyclicRead.c
+++ Grade5+/cyclicRead.c
@@ -23,13 +23,7 @@ int main()
 		return -1;
 	}
 
-	uint64_t pos = (mem->pos + 64) % 128;
-	//uint64_t pos = 0;
-
-	uint64_t oldPos = pos;
-
-	int posTime = 0;
-	int memPosTime = 0;
+	uint64_t pos = 0;
 
 	printf("starting at %ld\n", pos);
 
@@ -39,32 +33,17 @@ int main()
 			continue;
 		}
 
+		if(pos + 128 <= mem->pos) {
+			break;
+		}
+
 		int64_t current_seed = verify((void *)mem->buffer[pos]);
 		if(current_seed == -1) {
 			break;
 		}
 
-		if(mem->pos == 127) { //count every rotation of writer
-			memPosTime++;
-		}
-
-		if(pos == 127) { //count every rotation of reader
-			posTime++;
-		}
-
-		if(memPosTime > posTime) { //if writer has outrunned reader, do something with reader's pos (acknowledge reader by some way - resynchronizing (start from beginning) or break)
-			//pick one of them :D
-
-			//pos = 0;
-			pos = mem->pos;
-			//pos = oldPos; (uncomment part 1)
-			//break;
-		}
-
+		printf("Parsed and verified data block: %ld\n", pos);
 		pos++;
-		pos %= 128;
-
-		//oldPos = pos; (uncomment part 2)
 	}
 
 	return 0;
--- Grade5+/cyclicWrite.c
+++ Grade5+/cyclicWrite.c
@@ -31,18 +31,21 @@ int main()
 		return -1;
 	}
 
-	mem->pos %= 128;
-	//mem->pos = 0;
+	mem->pos = 0;
 
 	uint32_t seed = 0; //for function generate (see gen.h)
 
 	while(true) {
 		generate((void*)mem->buffer[mem->pos], seed);
-
+		printf("Generate data block: %ld\n", mem->pos);
 		mem->pos++;
-		mem->pos %= 128;
+		mem->pos %= 128; //this has to stay here because first we generate and then we verify; if it is >= 128 -> memory leak
 
 		seed++;
+
+		if(mem->pos % 1 == 0) { //see 10 data blocks per 1 second
+			usleep(70000);
+		}
 	}
 
 	return 0;
